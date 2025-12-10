591c6497e5815040827f8676d44f5c36e649523b
Flemming Eriksson <fleeri91@gmail.com>
2025-12-10 14:28:42 +0100

    prettier formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ["./app/assets/css/main.css"],
+  css: ['./app/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
   },
-});
+})
