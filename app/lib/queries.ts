b09e4a8561afdb2f8f9dffc79ed3c5f882083af6
Flemming Eriksson <fleeri91@gmail.com>
2025-12-12 20:38:17 +0100

    optimize query

--- app/lib/queries.ts
+++ app/lib/queries.ts
@@ -1,11 +1,20 @@
 // lib/queries.ts
 import { useQuery } from '@tanstack/vue-query'
+import type { MyTeamRoot } from '~/types/MyTeam'
 
-export const useExampleQuery = () => {
+export const useEntryQuery = (entryId: Ref<string>) => {
   return useQuery({
-    queryKey: ['example'],
-    //queryFn: () => exampleFetcher(),
-    staleTime: 1000 * 60 * 5,
+    queryKey: ['entry'], // no reactive dependency
+    enabled: false, // DO NOT auto-run
+    retry: false,
     refetchOnWindowFocus: false,
+    queryFn: async () => {
+      if (!entryId.value) throw new Error('Missing entryId')
+
+      return await $fetch<MyTeamRoot>('/api/optimize-team', {
+        method: 'GET',
+        query: { entryId: entryId.value },
+      })
+    },
   })
 }
